ac90d76e342331805af8eb9a49f872067821a4ab
SubhrajyotiSen <subhrajyoti12@gmail.com>
2023-07-01 12:17:24 +0530

    Remove old projects

--- 08-finish-your-view-holder/starter/Listmaker/app/src/main/java/com/raywenderlich/listmaker/TodoListViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.raywenderlich.listmaker
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class TodoListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-}
--- 42-use-a-viewmodel/starter/Listmaker/app/src/main/java/com/raywenderlich/listmaker/TaskListViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.raywenderlich.listmaker
-
-import android.view.View
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-
-class TaskListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val taskTextView = itemView?.findViewById<TextView>(R.id.textview_task) as TextView
-}
